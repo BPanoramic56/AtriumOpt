e40256f33578b1f96d9cb5147f60d9feea8cc370
brunogomespascotto <panoramic56@proton.me>
2024-06-09 00:03:24 -0600

    Added returned values that were removed when remaking gitignore

--- CardModifier.py
+++ CardModifier.py
@@ -8,15 +8,23 @@ import SQLHandler
 
 config = configparser.ConfigParser()
 config.read("configurations.ini")
+Possible_Access = config(["Access Information"]["Possible"]).split(", ")
 
 if __name__ == "__main__":
     while True:
-        card    = input("Type the new card ID(s): ")
         access  = input("Type the new card Access(s): ")
-        user    = input("Type the new card User: ")
+        exit = False
+        
+        for single_access in access:
+            if single_access not in Possible_Access:
+                print(f"The given access contains invalid words: {single_access}")
+                exit = True
 
-        if len(access) == 1:
-            access = "CONF" + access
+        if exit == True:
+            continue
+
+        card    = input("Type the new card ID(s): ")
+        user    = input("Type the new card User: ")
 
         for card_id in card:
             SQLHandler.__init__(card_id, access, user)
